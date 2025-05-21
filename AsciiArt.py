cbdb11722a4c71255e1ba2e41a7bb03883e6ee96
Mike Kabbabe <83938405+33Mic@users.noreply.github.com>
2025-05-20 21:44:53 -0600

    Made changes to fit aspect ratio better
    Provided customizable max height

--- AsciiArt.py
+++ AsciiArt.py
@@ -10,30 +10,36 @@ def pixelToGrayScale(pixels: list[list], j: int, i: int)-> None:
 
 def pixelToAscii(pixels: list[list], j: int, i: int)-> str:
     ramp = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
-    return ramp[int(pixels[j, i][0] * len(ramp) / 256)]
+    return ramp[int(pixels[j, i][0] * len(ramp) / 256)] + "  "
 
-def detectSize(img: Image)-> tuple[int, int]:
-    MAX_SIZE = 128
+def detectSize(img: Image, max_size: int)-> tuple[int, int]:
     width, height = img.size
-    if(width <= MAX_SIZE or height <= MAX_SIZE):
+    if(width <= max_size or height <= max_size):
         print(f"Image is {width}x{height}.")
         return (width, height)
     else:
         print(f"Image is {width}x{height}.")
-        print(f"Image is too large. Resizing to fit {MAX_SIZE}x{MAX_SIZE} with respect to aspect ratio.")
+        print(f"Image is too large. Resizing to fit {max_size}x{max_size} with respect to aspect ratio.")
         if(width > height):
-            return (MAX_SIZE, MAX_SIZE * height // width)
+            return (max_size, max_size * height // width)
         else:
-            return (MAX_SIZE * width // height, MAX_SIZE)
+            return (max_size * width // height, max_size)
     
 def main():
+    max_size = 0
     try:
         img = Image.open(input("Enter image path: "))
     except:
         print("Invalid path")
         return
     
-    img = img.resize(detectSize(img))
+    try:
+        max_size = int(input("Please specify maximum image height (in pixels): "))
+    except:
+        print("Not a number.")
+        return
+    
+    img = img.resize(detectSize(img, max_size))
     pixels = img.load()
 
     # Convert to grayscale
